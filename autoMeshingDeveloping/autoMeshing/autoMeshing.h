0fbe4f6c8375e0827a27fef31146abe0096d6f00
HP <onouedod@gmail.com>
2025-02-04 17:00:29 +0900

    add develop repository

---
+++ autoMeshingDeveloping/autoMeshing/autoMeshing.h
@@ -0,0 +1,10 @@
+// autoMeshing.h
+#ifndef AUTOMESHING_H
+#define AUTOMESHING_H
+
+namespace autoMeshing {
+    void test();
+}
+
+#endif
+
---
+++ autoMeshingDeveloping/autoMeshing/console.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <autoMeshing.h>
+
+namespace autoMeshing {
+    void test() {
+        std::cout << "Hello World!\n";
+    }
+}
